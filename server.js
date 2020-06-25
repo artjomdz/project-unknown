6ae7c87f800f039f1af2f1169210ac2243e234a2
Artjom Dzug <adzug@deloittece.com>
2020-06-25 14:19:45 +0200

    Added controller/models/middleware

--- server.js
+++ server.js
@@ -3,6 +3,57 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const db = require('./models');
+const dbConfig = require('./config/db.config');
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Successfully connected to DB');
+        initial();
+    })
+    .catch(err => {
+        console.log('Connection error', err);
+        process.exit();
+    });
+
+function initial () {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: 'User'
+            }).save(err => {
+                if(err) {
+                    console.log('error'. err);
+                }
+                console.log('Added user to role');
+            });
+
+            new Role({
+                name: 'moderator'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err);            
+                }
+                console.log('Added moderator to role')
+            })
+
+            new Role({
+                name: 'admin'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err);            
+                }
+                console.log('Added admin to role')
+            })
+        }
+    })
+}
+
 
 var corsOptions = {
     origin : 'http://localhost:8081'
@@ -21,6 +72,3 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
 
-app.get('/time', (req, res) => {
-    res.json({Time: new Date()});
-})
